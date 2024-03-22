de7f962f214ddd2182e9ad23dee4613f04cbef5c
togay-aytemiz <togayaytemiz@gmail.com>
2024-03-22 21:17:13 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import "./styles.css";
+export default function App() {
+  const [userInput, setUserInput] = useState("");
+  const [trackingReports, setTrackingReports] = useState([]);
+
+  const infractionMessage = "🚨🚨🚨 İHLAL TESPİT EDİLDİ! 🚨🚨🚨";
+
+  if (trackingReports.length > 0) {
+    console.log(trackingReports);
+    if (trackingReports[trackingReports.length - 1].infractionDetected) {
+      console.log(infractionMessage);
+    }
+  }
+
+  function getTimeStamp() {
+    const timeStamp = new Date();
+    return (
+      timeStamp.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+      }) +
+      "." +
+      (timeStamp.getMilliseconds() / 1000).toFixed(3).slice(-3)
+    );
+  }
+
+  /* Challenge 
+
+Bu şirket çalışanlarını gözetlemek istiyor. Göreviniz aşağıdakileri yapmalarına yardımcı olmak:
+
+    1. Kullanıcı textarea'ya her yazı yazdığında, userInput ve trackingReports state'lerinin her ikisi de güncellenmelidir. 
+       
+            a. userInput'un değeri, kullanıcının textarea'ya yazdığı her şeye eşit bir string olmalıdır (aşağıdaki görev 2'de tartışılan bir istisna dışında). 
+            
+            b. trackingReports state array için, dizide önceden var olan tüm nesneler korunmalı ve array'in sonuna yeni bir nesne eklenmelidir. 
+                    
+             Özellik   		 	          Değer 				  
+		    	╷----------------------╷-------------------------------------------╷
+		      |  timeStamp           |  getTimeStamp fonksiyonunun return değeri |
+		    	|----------------------|-------------------------------------------|
+		    	|  employeeInput       |  textarea'daki geçerli girdinin tümü      |
+		    	|----------------------|-------------------------------------------|
+		    	|  infractionDetected  |   employeeInput "Evil Corp." stringini    |
+          |                      |  içeriyorsa true - aksi takdirde, false   |	
+		    	¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+           
+       2. Eğer kullanıcı "Evil Corp." metnini yazarsa ("title case" olarak) yazarsa, bu dize hem userInput state'inde hem de textarea'da otomatik olarak "Good Corp." ile değiştirilmelidir. 
+       
+    3. Metin alanına "Evil Corp. test" yazarak uygulamayı test edin. Bu görevleri doğru bir şekilde tamamlarsanız, her harf yazdığınızda bir console.log mesajı almalısınız ve mesajlar sampleOutput.md dosyasındakiler gibi olmalıdır.
+
+       
+       4. Yalnızca aşağıdaki kodu yazmanız gerekir. Yukarıdaki veya projenin başka bir yerindeki kodların hiçbirinin değiştirilmesi gerekmiyor.
+*/
+
+  function findEvilCorp(reports) {
+    return reports.some((report) =>
+      report.employeeInput.toLowerCase().includes("evil corp.")
+    );
+  }
+
+  const handleChange = (e) => {
+    let value = e.target.value;
+    let evilCorpDetected = findEvilCorp(trackingReports);
+
+    if (value.toLowerCase().includes("evil corp.")) {
+      value = value.replace(/evil corp\./gi, "Good Corp.");
+      console.log(infractionMessage);
+    }
+
+    setUserInput(value);
+    setTrackingReports([
+      ...trackingReports,
+      {
+        timeStamp: getTimeStamp(),
+        employeeInput: value,
+        infractionDetected: evilCorpDetected,
+      },
+    ]);
+  };
+
+  return (
+    <div>
+      <Header />
+      <textarea
+        placeholder="Raporunuzu buraya yazın..."
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
